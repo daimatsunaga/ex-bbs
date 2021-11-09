2e074dc0e1a8ab611c78f2c4a48ab376bc74fbcd
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-09 15:23:09 +0900

    ArticleRepository (findAllメソッドの作成)

---
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -0,0 +1,27 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Article;
+
+@Repository
+public class ArticleRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	private static final RowMapper<Article> ARTICLE_ROW_MAPPER
+	 = new BeanPropertyRowMapper<>(Article.class);
+	
+	public List<Article> findAll() {
+		String sql = "SELECT * FROM articles;";
+		List<Article> articleList = template.query(sql, ARTICLE_ROW_MAPPER);
+		return articleList;
+	}
+}
