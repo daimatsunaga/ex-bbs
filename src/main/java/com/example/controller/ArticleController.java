541d0a7c59e6ad06ce0c050049201e947a4581eb
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-09 15:42:26 +0900

    ArticleController(insertメソッドの作成)

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
 import com.example.repository.ArticleRepository;
 
 @Controller
@@ -26,6 +28,14 @@ public class ArticleController {
 		List<Article> articleList = articleRepository.findAll();
 		session.setAttribute("articleList", articleList);
 		return "article_index";
+	}
+	
+	@RequestMapping("/insert")
+	public String insert(ArticleForm form) {
+		Article article = new Article();
+		BeanUtils.copyProperties(form, article);
+		articleRepository.insert(article);
+		return "redirect:/article";
 	}
 	
 	
